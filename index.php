030477e8fb285046a1f938be3c72013c114ddc1a
Oleksandr-eng <oleksandr.sereda11@gmail.com>
2021-02-06 16:49:27 +0200

    ddddd

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="ua">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Practice</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<?php
+echo 'Hello, NIX Education';
+?>
+<header class="header">
+    <div class="wrapper">
+        <div class="header_wrapper">
+            <div class="header_allrounder">
+                <a href="#!" class="header_allrounder-link">
+                    <img src="./img/png/ALLROUNDER.png" alt="Website Tamplate" class="header_allrounder-pic">
+                </a>
+            </div>
+
+            <nav class="header_nav">
+                <ul class="header_list">
+                    <li class="header_item">
+                        <a href="#!" class="header_link">Lesson 1</a>
+                    </li>
+                    <li class="header_item">
+                        <a href="#!" class="header_link"></a>
+                    </li>
+                    <li class="header_item">
+                        <a href="#!" class="header_link">Lesson 2</a>
+                    </li>
+                    <li class="header_item">
+                        <a href="#!" class="header_link">Lesson 3</a>
+                    </li>
+                 </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<hr>
+<main class="main">
+    <section class="intro">
+        <h1 class="content">
+            <p>
+                The main content. dvsjvnjkanva
+            </p>
+        </h1>
+    </section>
+</main>
+<footer class="footer">
+    &copy; Oleksandr Sereda
+</footer>
+</body>
+</html>
